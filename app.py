83059e9113e5aecdfcc49eb01a1d3d60a9faca85
Nick HS <nickhs@umich.edu>
2012-10-12 00:51:27 -0700

    MVP. Will shit itself

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, session, render_template, request, jsonify, url_for, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form, TextField, PasswordField
+import requests
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py', silent=False)
 
 db = SQLAlchemy(app)
-
-articles = db.Table('articles',
-                    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-                    db.Column('article_id', db.Integer, db.ForeignKey('article.id'))
-                    )
 
 
 class User(db.Model):
@@ -19,8 +15,13 @@ class User(db.Model):
     name = db.Column(db.String)
     password = db.Column(db.String)
 
-    articles = db.relationship('Article', secondary=articles,
-                               backref=db.backref('users', lazy='dynamic'))
+    shares = db.relationship('Share', backref='receiver',
+                             primaryjoin="User.id==Share.receiver_id",
+                             lazy='dynamic')
+
+    shared = db.relationship('Share', backref='originator',
+                             primaryjoin="User.id==Share.originator_id",
+                             lazy="dynamic")
 
     def __init__(self, email, password):
         self.email = email
@@ -40,8 +41,11 @@ class Article(db.Model):
     text = db.Column(db.Text)
     icon = db.Column(db.LargeBinary)
     author = db.Column(db.String)
-    excerpt = db.Column(db.String(500))
+    excerpt = db.Column(db.String(250))
     parsed = db.Column(db.Boolean)
+
+    shares = db.relationship('Share', backref='article',
+                             lazy='dynamic')
 
     def __init__(self, link, parsed=False):
         self.link = link
@@ -62,9 +66,25 @@ class Article(db.Model):
         return self.title
 
 
-class NewArticleForm(Form):
-    link = TextField('Link')
-    friends = TextField('Friends')
+class Share(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
+    read = db.Column(db.Boolean)
+    comments = db.Column(db.Text)
+
+    def __init__(self, originator, receiver, article, read=False):
+        self.originator_id = originator.id
+        self.receiver_id = receiver.id
+        self.article = article
+        self.read = read
+
+    def __repr__(self):
+        return "<Share: %r>" % self.id
+
+    def __str__(self):
+        return self.id
 
 
 class NewUserForm(Form):
@@ -75,6 +95,11 @@ class NewUserForm(Form):
 class LoginForm(Form):
     email = TextField("Email")
     password = PasswordField("Password")
+
+
+class NewShareForm(Form):
+    link = TextField("Link")
+    friends = TextField("Friends")
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -88,7 +113,11 @@ def home():
 
         return render_template('landing.html', form=form)
     else:
-        return render_template('index.html')
+        id = session.get('user_id')
+        user = User.query.get(id)
+        # TODO: Order by date, pagination
+        shares = user.shares.all()
+        return render_template('index.html', user=user, shares=shares)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -98,28 +127,83 @@ def register():
         user = User(form.email.data, form.password.data)
         add_item(user)
         return redirect(url_for('home'))
-    else:
-        print "Did not validate"
 
     return render_template('signup.html', form=form)
 
 
-@app.route('/api/user/<id>/articles/new', methods=['POST'])
-def add_new_article(id):
-    form = request.form
+@app.route('/share', methods=['GET', 'POST'])
+def share():
+    form = NewShareForm()
     if form.validate_on_submit():
-        article = Article.query.filter_by(link=form.data.link).first()
+        article = Article.query.filter_by(link=form.link.data).first()
+        user = User.query.get(session.get('user_id'))
 
         if article is None:
-            article = Article(form.data.link)
+            article = Article(form.link.data)
             add_item(article)
             process_article(article)
 
-        return jsonify({'status': 'okay', 'link': article.link, 'id': article.id})
+        friends = form.friends.data.split(' ')
+        for email in friends:
+            friend = User.query.filter_by(email=email).first()
+
+            if friend is None:
+                # TODO; Handle missing friend
+                continue
+
+            share = Share(user, friend, article)
+            add_item(share)
+
+        return redirect(url_for('home'))
+
+    return render_template('share.html', form=form)
+
+
+@app.route('/article/<id>')
+def view_article(id):
+    article = Article.query.get(id)
+    return render_template('article.html', article=article)
+
+
+@app.route('/api/user/<id>/articles/new', methods=['POST'])
+def add_new_article(id):
+    data = request.json
+    user = User.query.get_or_404(id)
+    article = Article.query.filter_by(link=data['link']).first()
+
+    if article is None:
+        article = Article(data['link'], user)
+        process_article(article)
+    else:
+        article.add_user(user)
+
+    add_item(article)
+    return jsonify({'status': 'okay', 'link': article.link, 'id': article.id})
 
 
 def process_article(article):
-    pass
+    url = app.config['DIFFBOT_ENDPOINT']
+
+    params = {
+        'token': app.config['DIFFBOT_TOKEN'],
+        'url': article.link,
+        'summary': True,
+        'stats': True,
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == requests.codes.ok:
+        article.text = response.json.get('text')
+        article.title = response.json.get('title')
+        article.author = response.json.get('author')
+        print response.json.get('tags')
+        print response.json.get('stats')
+        print response.json.get('summary')
+        article.parsed = True
+        add_item(article)
+
+        return response
 
 
 def add_item(item):
